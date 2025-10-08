b02a000af6c652d783a586998f77df73a4be0480
gilper0x <gilper0x@gmail.com>
2025-10-08 13:10:33 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -34,13 +34,13 @@ green='\e[0;32m'
 yell='\e[1;33m'
 tyblue='\e[1;36m'
 purple='\e[0;35m'
-NC='\e[0m'
+nc='\e[0m'
 purple() { echo -e "\\033[35;1m${*}\\033[0m"; }
 tyblue() { echo -e "\\033[36;1m${*}\\033[0m"; }
 yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
 green() { echo -e "\\033[32;1m${*}\\033[0m"; }
 red() { echo -e "\\033[31;1m${*}\\033[0m"; }
-
+mkdir -p /usr/local/sbin
 
 red='\e[1;31m'
 green='\e[0;32m'
