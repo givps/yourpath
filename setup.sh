18e35cbc1ce1d85fb55885d54ecdc28f60ac7a93
gilper0x <gilper0x@gmail.com>
2025-10-08 15:26:08 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -41,6 +41,7 @@ yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
 green() { echo -e "\\033[32;1m${*}\\033[0m"; }
 red() { echo -e "\\033[31;1m${*}\\033[0m"; }
 mkdir -p /usr/local/sbin
+mkdir -p /usr/local/etc/xray
 
 red='\e[1;31m'
 green='\e[0;32m'
